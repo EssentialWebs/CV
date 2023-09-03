c9247436a8d110ba28f911f64591eb8f46867eb1
patodfernandez <patricio.dfernandez@gmail.com>
2023-09-03 18:26:17 -0300

    add server

--- src/firebase.js
+++ src/firebase.js
@@ -9,15 +9,15 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+ 
 const firebaseConfig = {
-  apiKey: "AIzaSyB9XtwaMX6GXCickWsRdFj9SWn-23ENIXI",
-  authDomain: "streaming-front-95aa5.firebaseapp.com",
-  databaseURL: "https://streaming-front-95aa5-default-rtdb.firebaseio.com",
-  projectId: "streaming-front-95aa5",
-  storageBucket: "streaming-front-95aa5.appspot.com",
-  messagingSenderId: "5178548171",
-  appId: "1:5178548171:web:71ee8d4631a61ec68ce04e",
-  measurementId: "G-T08PGP9CRX"
+  apiKey: "AIzaSyC7dNLyfRR0uPWSUhNUyK4VKq75tjTckKY",
+  authDomain: "pato-cv.firebaseapp.com",
+  projectId: "pato-cv",
+  storageBucket: "pato-cv.appspot.com",
+  messagingSenderId: "704259895722",
+  appId: "1:704259895722:web:1f6a227a84b8a9d4797bd3",
+  measurementId: "G-L29J7J0RSP"
 };
 
 
